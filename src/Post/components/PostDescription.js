7dbf5e7378281a2d2a47e7d4965dad777a855210
natachalevesque <natacha.levesque@outlook.com>
2022-01-10 12:41:02 -0500

    add space between text and read more/show less, update title

--- src/Post/components/PostDescription.js
+++ src/Post/components/PostDescription.js
@@ -8,8 +8,8 @@ function PostDescription({nasaDataDescription, naseDataTitle, nasaDataDate, clas
     return (
         <div className={`PostDescription ${className}`}>
             <h4 className={"Title"}>{naseDataTitle} - {nasaDataDate}</h4>
-            <p className={"Paragraph"}>{!readMore ? nasaDataDescription.slice(0, 200) + "..." : nasaDataDescription}
-                <button onClick={() => setReadMore(!readMore)}
+            <p className={"Paragraph"}>{!readMore ? nasaDataDescription.slice(0, 200) + "..." : nasaDataDescription}&nbsp;
+                <button type={"button"} onClick={() => setReadMore(!readMore)}
                         className={"ReadMore"}>{!readMore ? "Read More" : "Show Less"}</button>
             </p>
         </div>
